e1be7bff346c7d765af75c9135812591e8a4371d
Sec <chrsec@qq.com>
2019-11-04 17:20:41 +0800

    Create 18-Import.go

---
+++ 18-Import.go
@@ -0,0 +1,36 @@
+package main
+
+/*
+方法一
+import "fmt"
+import "os"
+*/
+
+/*
+方法二（常用）
+import （
+	"fmt"
+	"os"
+）
+*/
+
+/*方法三
+.操作
+调用函数无需通过包名
+import . "fmt"
+*/
+
+/*
+给包起别名
+import io "fmt"
+*/
+
+/*
+忽略此包
+import _ "fmt"
+引入该包，不直接使用包内函数，调用包内init函数
+*/
+
+func main() {
+
+}
