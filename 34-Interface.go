3b29fc72bdee37382855fd0237f7b75ee1c76547
Sec <chrsec@qq.com>
2019-11-12 21:50:09 +0800

    Update 34-Interface.go

--- 34-Interface.go
+++ 34-Interface.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 //定义接口
+//子集
 type Humaner interface {
 	//方法，只声明不实现，由别的类型（自定义类型）实现
 	sayHi()
+}
+
+//超集
+type Humaner1 interface {
+	Humaner//匿名字段，继承Humaner接口
+	sing(s string)
 }
 
 type Student1 struct {
@@ -18,6 +25,10 @@ type Teacher1 struct {
 
 func (temp *Student1)sayHi(){
 	fmt.Println("student name = ",temp.name)
+}
+
+func (temp *Student1)sing(s string){
+	fmt.Println("student sing = ",s)
 }
 
 func (temp *Teacher1)sayHi(){
@@ -54,4 +65,22 @@ func main() {
 	for _,value := range x{
 		value.sayHi()
 	}
+
+	var i1 Humaner1
+	s2:=&Student1{"mike"}
+	i1 = s2
+	i1.sayHi()//继承过来的方法
+	i1.sing("song")
+
+	//超集可以转化为子集，子集不能转化为超集
+	i = i1
+	fmt.Print("超集转化为子集：")
+	i.sayHi()
+
+	//空接口 万能类型，可以保存任意类型的值
+	//空接口不包含任何方法，所有类型都实现空接口
+	//如println（arg...interface）
+	var i3 interface{} = 1
+	fmt.Println("i3 = ",i3)
+
 }
